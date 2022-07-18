47a8938533cac288ea126037a573547cb150bdfa
DaNiels89 <nielssloesen@hotmail.com>
2022-07-18 21:32:59 +0200

    first commit

---
+++ resources/views/sections/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="content-info">
+  This is the footer!
+  {{-- @php(dynamic_sidebar('sidebar-footer')) --}}
+</footer>
---
+++ resources/views/sections/header.blade.php
@@ -0,0 +1,16 @@
+<header class="banner">
+  <div class="stickybar">
+    <p class="stickybar__text">Enjoy a festive, golf-centered summer day. There are still some spots left!</p>
+    <button class="stickybar__button">Download info kit</button>
+  </div>
+  
+  <!-- <a class="brand" href="{{ home_url('/') }}">
+    {{-- {!! $siteName !!} --}}
+  </a> -->
+
+  {{-- @if (has_nav_menu('primary_navigation'))
+    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+    </nav>
+  @endif --}}
+</header>
